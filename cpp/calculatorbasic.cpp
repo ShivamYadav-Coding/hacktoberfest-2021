9c8d9ef134a71b360e5831e716c40b0271b9f14c
nira07 <63848451+nira07@users.noreply.github.com>
2021-10-04 17:37:58 +0530

    basic calculator app code also in exe file

---
+++ cpp/calculatorbasic.cpp
@@ -0,0 +1,37 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator either + or - or * or /: ";
+    cin >> op;
+
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;switch(op)
+    {
+        case '+':
+            cout << num1+num2;
+            break;
+
+        case '-':
+            cout << num1-num2;
+            break;
+
+        case '*':
+            cout << num1*num2;
+            break;
+
+        case '/':
+            cout << num1/num2;
+            break;
+
+        default:
+           
+            break;
+    }
+
+    return 0;
+}//thank you for reviewing this
